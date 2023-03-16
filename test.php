ac609c82de3abee3a10f3b67729ef78a2e825b2c
MattheusCatoen <91245158+MattheusCatoen@users.noreply.github.com>
2023-03-16 15:54:35 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,3 @@
 <?php
-// Récupère le code HTML de la page actuelle
-$html = file_get_contents('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-
-// Affiche le code HTML récupéré
-echo $html;
+phpinfo();
 ?>
